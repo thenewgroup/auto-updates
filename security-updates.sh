176f1333abd895fcd073df3c3f12b17c5b59fc15
Greg Boggs <gregboggs@153069.no-reply.drupal.org>
2015-03-17 11:22:17 -0700

    initial commit of a security update script

---
+++ security-updates.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Place this script in cron and run once per hour.
+
+WEBROOT="/var/www/html"
+EMAIL="me@example.com"
+SITE_NAME="example.com"
+
+cd $WEBROOT
+
+# Make sure status is up to date
+ ~/.composer/vendor/bin/drush pm-refresh
+
+# Check for Security Updates
+OUTPUT="$(drush pm-updatestatus --security-only)"
+if [[ $OUTPUT == *"UPDATE"* ]]
+then
+  drush vset maintance_mode 1
+
+  # Take a backup and if it succeeds, run the update
+  drush sql-dump | gzip > ~/backup/prod.sql.gz && drush up --security-only -y
+  drush vset maintance_mode 0
+
+  # Notify stakeholders
+  echo "A critical security update has been applied to $SITE_NAME. You should test production now." | mail -s "Your website needs testing" "$EMAIL";
+fi
