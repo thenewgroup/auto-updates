1bad343129fdaf5bf3e3811166c2f43bd40807fa
sco_tt <scott@scottpinkelman.com>
2015-03-18 09:03:00 -0400

    make the email dependent on a sucessfull software update

--- security-updates.sh
+++ security-updates.sh
@@ -32,10 +32,10 @@ do
 		then
 			drush vset maintance_mode 1
 			# Take a backup and if it succeeds, run the update
-			drush sql-dump | gzip > ${BACKUP_DIR}/${i}-pre-sec-update.sql.gz && drush up --security-only -y
+			drush sql-dump | gzip > ${BACKUP_DIR}/${i}-pre-sec-update.sql.gz && drush up --security-only -y && mail -s "Your website needs testing" "$EMAIL"
 			drush vset maintance_mode 0
 		  # Notify stakeholders
-			echo "A critical security update has been applied to $i. You should test production now." | mail -s "Your website needs testing" "$EMAIL";
+			echo "A critical security update has been applied to $i. You should test production now."
 		else
 			echo "No available security updates"
 		fi
