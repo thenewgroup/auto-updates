6492a834483402897e71a2965675e7bb711b75cb
sco_tt <scott@scottpinkelman.com>
2015-03-18 05:59:26 -0400

    first attempt at modifying script to scan webroot and update mutlipe drupal sites

--- security-updates.sh
+++ security-updates.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 # Place this script in cron and run once per hour.
 
-WEBROOT="/var/www/html"
+WEBROOT="/var/www"
 EMAIL="me@example.com"
-SITE_NAME="example.com"
 
+echo "Scanning sites directory for drupal installations"
 cd $WEBROOT
 
-# Make sure status is up to date
- drush pm-refresh
+for i in $(ls)
+do 
+	SITE_DIR=$(readlink -f $i)
+	# if your site files are in directories immediately
+	# beneath your site_dir, i.e. /var/www/site.com,
+	# then you don't need to `cd public_html'
+	# just use `cd $a` below
+	cd $a && cd public_html
+	echo $(pwd) 
+	# Make sure status is up to date
+	 drush pm-refresh
 
-# Check for Security Updates
-OUTPUT="$(drush pm-updatestatus --security-only)"
-if [[ $OUTPUT == *"UPDATE"* ]]
-then
-  drush vset maintance_mode 1
+	# Check for Security Updates
+	OUTPUT="$(drush pm-updatestatus --security-only)"
+	if [[ $OUTPUT == *"UPDATE"* ]]
+	then
+	  echo "Drupal site found"
+	  drush vset maintance_mode 1
 
-  # Take a backup and if it succeeds, run the update
-  drush sql-dump | gzip > ~/backup/prod.sql.gz && drush up --security-only -y
-  drush vset maintance_mode 0
+	  # Take a backup and if it succeeds, run the update
+	  drush sql-dump | gzip > ~/backup/prod.sql.gz && drush up --security-only -y
+	  drush vset maintance_mode 0
 
-  # Notify stakeholders
-  echo "A critical security update has been applied to $SITE_NAME. You should test production now." | mail -s "Your website needs testing" "$EMAIL";
-fi
+	  # Notify stakeholders
+	  echo "A critical security update has been applied to $SITE_NAME. You should test production now." | mail -s "Your website needs testing" "$EMAIL";
+	  else
+		echo "No Drupal site found in this directory"
+	  fi 
+	  cd $WEBROOT
+done
+echo "Done with Drupal Security Updates"
