2210eb54db12d4913f0ee8fcbdc15a502c2df44d
Greg Boggs <gregboggs@153069.no-reply.drupal.org>
2015-03-18 10:29:25 -0700

    Fix backups, and mail. Clean up comments, add directions in source code, add which drush command, adjust folder loop to work in my environment

--- security-updates.sh
+++ security-updates.sh
@@ -1,47 +1,61 @@
 #!/bin/bash
-# Place this script in cron and run once per hour.
+# Place this script in cron and run hourly. To limit this script to only 1 site, set WEB_ROOT to your Drupal root.
+#
+# To use on multiple sites, set WEB_ROOT at the shared folder for all Drupal sites.
 
-# run `which drush` to find this path
-drush='/usr/bin/drush'
-WEBROOT="/var/www"
+drush=`which drush`
+WEB_ROOT="/var/www/"
+
+# Replace with "public_html" if you use a public_html subfolder
+PUBLIC_DIR="."
 EMAIL="me@example.com"
-BACKUP_DIR = "~/backups/manual"
+BACKUP_DIR="$HOME/backups/"
 
-echo "Scanning sites directory for drupal installations"
-cd $WEBROOT
+# Create a backup folder if it does not exist.
+if [[ ! -d $BACKUP_DIR ]]
+then
+	echo "Creating new backup directory in $BACKUP_DIR"
+	mkdir -p $BACKUP_DIR
+fi
 
-for i in $(ls)
-do 
+echo "Scanning sites directory for Drupal installations"
+cd $WEB_ROOT
+
+for i in $WEB_ROOT/
+do
+
+	# Handle symlinks
 	SITE_DIR=$(readlink -f $i)
-	# if your site files are in directories immediately
-	# beneath your site_dir, i.e. /var/www/site.com,
-	# then you don't need to `cd public_html'
-	# just use `cd $a` below
-	cd $SITE_DIR && cd public_html
-	echo $(pwd) 
-	# first check to see if site directory has a drupal site
+	cd $SITE_DIR
+	  cd $PUBLIC_DIR
+
+	# Does the directory have a Drupal site?
 	SITE_STATUS=$($drush status | wc -l)
 	if [[ $SITE_STATUS -gt 7 ]]
-	then 
-		echo "Drupal site found"
+	then
+		echo "Drupal site found in $(pwd)"
+
 		# Make sure status is up to date
 		drush pm-refresh
-		# Check for Security Updates
+
+		# Check for security updates
 		OUTPUT="$(drush pm-updatestatus --security-only)"
 		if [[ $OUTPUT == *"UPDATE"* ]]
 		then
 			drush vset maintance_mode 1
+
 			# Take a backup and if it succeeds, run the update
-			drush sql-dump | gzip > ${BACKUP_DIR}/${i}-pre-sec-update.sql.gz && drush up --security-only -y && mail -s "Your website needs testing" "$EMAIL"
+			SITE_NAME=`basename ${i}`
+			drush sql-dump | gzip > ${BACKUP_DIR}/${SITE_NAME}-pre-sec-update.sql.gz && drush up --security-only -y | mail -s "Your website needs testing" "$EMAIL"
 			drush vset maintance_mode 0
-		  # Notify stakeholders
-			echo "A critical security update has been applied to $i. You should test production now."
+
+			# Notify stakeholders
+			echo "A critical security update has been applied to $SITE_NAME. You should test production now."
 		else
 			echo "No available security updates"
 		fi
 	else
-		echo "No Drupal Site Found"
-	fi 
-	cd $WEBROOT
+		echo "No Drupal site found"
+	fi
 done
-echo "Done with Drupal Security Updates"
+echo "Done with Drupal security updates"
