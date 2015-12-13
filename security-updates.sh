9acf91969388ec41e62fb7a9e7b79fe800ee6171
testbird <email.bug@arcor.de>
2015-12-13 14:15:16 +0100

    improved defaults + drupal 8 maintenance mode bugfix

--- security-updates.sh
+++ security-updates.sh
@@ -2,21 +2,27 @@
 # You may poll for updates by running this script in a cron job, or
 # have updates triggerd by piping security announcement emails into this script.
 # For example, with a ~/.forward or ~/.qmail-drupal file
-# that contains a line with: "|/path/to/this/script/named/security-updates.sh",
+# that contains a line reading (without the leading #):
+# |"/path/to/this/script/named/security-updates.sh mailpipe"
 # and subscribing $USER-drupal@$HOST to the security email newsletter.
 # https://www.drupal.org/security.
-#
+
 # To limit this script to only 1 site, set WEB_ROOT to your Drupal root.
-#
 # To use on multiple sites, set WEB_ROOT at the shared folder for all Drupal sites.
 
-drush=`which drush`
-WEB_ROOT="/var/www/"
+WEB_ROOT="/var/www"
 
 # Replace with "public_html" if you use a public_html subfolder
 PUBLIC_DIR="."
-EMAIL="me@example.com"
-BACKUP_DIR="$HOME/backups/"
+EMAIL="${USER}@${HOST}"
+BACKUP_DIR="$HOME/drush-backups" # should same place as drush uses
+
+# The drupal (command line) console is required to enable mainenance mode for drupal 8.
+
+# determine available commands
+drush=`which drush`
+drupal=`which drupal`
+
 
 # Create a backup folder if it does not exist.
 if [[ ! -d $BACKUP_DIR ]]
@@ -25,12 +31,15 @@ then
 	mkdir -p $BACKUP_DIR
 fi
 
-# Capture any message piped into this script and send it to $EMAIL.
-# Allows to trigger this script by directing security anouncement emails to it.
-stdin=$(cat)
-if [ -n "$stdin" ]
+# Capture message piped into this script and send it to $EMAIL.
+# This allows to trigger this script by directing security anouncement emails to it.
+if [ $1 == "mailpipe" ]
 then
- echo "$stdin" | mail -s "$WEB_ROOT: security-updates.sh got triggered with a message" "$EMAIL"
+    	stdin=$(cat)
+        if [ -n "$stdin" ]
+        then
+            	echo "$stdin" | mail -s "$WEB_ROOT: security-updates.sh mailpipe triggered" "$EMAIL"
+        fi
 fi
 
 
@@ -58,12 +67,25 @@ do
 		OUTPUT="$(drush pm-updatestatus --security-only)"
 		if [[ $OUTPUT == *"UPDATE"* ]]
 		then
-			drush vset maintance_mode 1
+			# enable  maintenance
+                        if [ -z "$drupal" ]
+                        then
+                            	drush vset maintance_mode 1     # this does not work with drupal 8
+                        else
+                            	drupal site:maintenance ON
+                        fi
 
 			# Take a backup and if it succeeds, run the update
 			SITE_NAME=`basename ${i}`
-			drush sql-dump | gzip > ${BACKUP_DIR}/${SITE_NAME}-pre-sec-update.sql.gz && drush up --security-only -y | mail -s "Your website needs testing" "$EMAIL"
-			drush vset maintance_mode 0
+			drush sql-dump | gzip > ${BACKUP_DIR}/${USER}-${SITE_NAME}-pre-sec-update_$(date +%F_%T).sql.gz && drush up --security-only -y | mail -s "${USER}-${SITE_NAME} website needs testing" "$EMAIL"
+
+                        # disable  maintenance
+                        if [ -z "$drupal" ]
+                        then
+                            	drush vset maintance_mode 0     # this does not work with drupal 8
+                        else
+                            	drupal site:maintenance OFF
+                        fi
 
 			# Notify stakeholders
 			echo "A critical security update has been applied to $SITE_NAME. You should test production now."
